8ab9d71def3d956c5884774cbff60af8aca14e21
wd2010 <wendan516033593@163.com>
2018-09-27 00:05:42 +0800

    puppeteer

---
+++ app.js
@@ -0,0 +1,37 @@
+import puppeteer from 'puppeteer';
+import path from 'path';
+const devices = require('puppeteer/DeviceDescriptors');
+const pathToExtension = path.join(__dirname, './chrome/chrome.exe');
+(async () => {
+  const browser = await puppeteer.launch({
+    executablePath: pathToExtension,
+    headless: false,
+    args: [ '--proxy-server=127.0.0.1:1080' ]
+  });
+  const page = await browser.newPage();
+  await page.goto('https://developers.google.com/web/');
+
+  // Type into search box.
+  await page.type('#searchbox input', 'Headless Chrome');
+
+  // Wait for suggest overlay to appear and click "show all results".
+  const allResultsSelector = '.devsite-suggest-all-results';
+  await page.waitForSelector(allResultsSelector);
+  await page.click(allResultsSelector);
+
+  // Wait for the results page to load and display the results.
+  const resultsSelector = '.gsc-results .gsc-thumbnail-inside a.gs-title';
+  await page.waitForSelector(resultsSelector);
+
+  // Extract the results from the page.
+  const links = await page.evaluate(resultsSelector => {
+    const anchors = Array.from(document.querySelectorAll(resultsSelector));
+    return anchors.map(anchor => {
+      const title = anchor.textContent.split('|')[0].trim();
+      return `${title} - ${anchor.href}`;
+    });
+  }, resultsSelector);
+  console.log(links.join('\n'));
+
+  await browser.close();
+})();
